ac1ab8832f0e506c09916131287f9696a7c3462e
BryanLeite-dev <79052818+EduardoDarkP8@users.noreply.github.com>
2022-11-17 08:38:56 -0300

    Menu Atual

--- Assets/Animação/Buttons.cs
+++ Assets/Animação/Buttons.cs
@@ -6,9 +6,41 @@ public class Buttons : MonoBehaviour
 {
     public Animator Anima;
     public Animator Anima2;
+    public Animator Anima3;
+    public Animator Anima4;
+    public Animator Anima5;
     public void NewGame()
     {
         Anima.SetBool("SelectNewGame",true);
         Anima2.SetBool("Ativar", true);
+        Anima.SetBool("Credits", false);
+        Anima4.SetBool("Ativar", true);
+        Anima5.SetBool("Ativar", false);
+    }
+    public void Credits() 
+    {
+        Anima.SetBool("Credits", true);
+        Anima2.SetBool("Ativar", true);
+        StartCoroutine(Cooldown(0.2f));
+        Anima4.SetBool("Ativar", true);
+        Anima5.SetBool("Ativar", false);
+    }
+    public void Back() 
+    {
+        Anima.SetBool("SelectNewGame", false);
+        Anima2.SetBool("Ativar", false);
+        Anima.SetBool("Credits", false);
+        Anima4.SetBool("Ativar", false);
+        Anima3.SetBool("Ativar", false);
+        Anima5.SetBool("Ativar", true);
+    }
+    public void Exit() 
+    {
+        Application.Quit();
+    }
+    public IEnumerator Cooldown(float time) 
+    {
+        yield return new WaitForSeconds(time);
+        Anima3.SetBool("Ativar", true);
     }
 }
