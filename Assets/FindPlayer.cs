6fc16a3f66daf5e4df7d46a90d8b73189b55ab0b
BryanLeite-dev <79052818+EduardoDarkP8@users.noreply.github.com>
2022-11-22 08:34:02 -0300

    Camera

---
+++ Assets/FindPlayer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FindPlayer : MonoBehaviour
+{
+    CinemachineVirtualCamera cn;
+    void Start()
+    {
+        cn = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    void Update()
+    {
+        if (GameObject.Find("Body") != null)
+        {
+            cn.Follow = GameObject.Find("Body").transform;
+        }
+    }
+}
--- Assets/Vivo.cs
+++ Assets/Vivo.cs
@@ -7,6 +7,7 @@ public class Vivo : MonoBehaviour
     public int Vida;
     public int Pv_C;
     public int PvMax;
+    public bool isPlayer;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,7 @@ public class Vivo : MonoBehaviour
     }
     void Morrer() 
     {
-        if (Vida <= 0) 
+        if (Vida <= 0 && isPlayer) 
         {
             GameObject ob = GameObject.Find("Hud") as GameObject;
             ob.transform.Find("Menu").gameObject.SetActive(true);
