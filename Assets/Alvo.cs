627fb69a813510fbfd4b3e774bfcd5218eb83c7f
BryanLeite-dev <79052818+EduardoDarkP8@users.noreply.github.com>
2022-11-24 08:35:57 -0300

    Animações de ataque

---
+++ Assets/Alvo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alvo : MonoBehaviour
+{
+
+    public GameObject[] Alvos = new GameObject[0];
+    public bool[] confirm = new bool[0];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FindObjects() 
+    {
+        
+    }
+}
